9b5df7faca13356e4a98bdb632849ad8587ca0ba
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2023-03-07 19:05:50 -0500

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+}
